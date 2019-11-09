a730187b2ce03ca51766fbb4d09cbbf842c1ca44
Raj Singh <rajsinghjat@gmail.com>
2019-11-09 12:23:20 +0530

    Update HandsOn4.go

--- HandsOn4.go
+++ HandsOn4.go
@@ -1,1 +1,21 @@
 
+package main
+
+import (
+	"fmt"
+)
+
+type myvar int
+
+//var b int
+
+var x myvar
+
+func main() {
+
+	fmt.Println(x)
+	fmt.Printf("%T", x)
+	x = 42
+	fmt.Println("\n After Assigment", x)
+	//	b = x
+}
