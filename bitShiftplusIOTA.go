ef509ac541177a52954875ee4c3e20db40bf927c
Raj Singh <rajsinghjat@gmail.com>
2019-11-10 11:16:57 +0530

    Update bitShiftplusIOTA.go

--- bitShiftplusIOTA.go
+++ bitShiftplusIOTA.go
@@ -1,1 +1,23 @@
 
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
+
+func main() {
+	x := 4
+	fmt.Printf("%d \t %b \n", x, x)
+	y := x << 1
+	fmt.Printf("%d \t %b\n", y, y)
+	fmt.Printf("%d \t %b\n", kb, kb)
+	fmt.Printf("%d \t %b\n", mb, mb)
+	fmt.Printf("%d \t %b\n", gb, gb)
+}
