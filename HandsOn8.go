51e4779af359fe32a8eafa9999a6db38ba479502
Raj Singh <rajsinghjat@gmail.com>
2019-11-10 11:36:32 +0530

    Update HandsOn8.go

--- HandsOn8.go
+++ HandsOn8.go
@@ -1,1 +1,18 @@
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	g := 42 == 42
+	h := 42 <= 42
+	i := 42 >= 42
+	j := 42 != 42
+	k := 42 < 42
+	l := 42 > 42
+
+	fmt.Println(g, h, i, j, k, l)
+
+}
